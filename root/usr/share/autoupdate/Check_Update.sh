97776e3dfb79655f7360da37ce076fe79050a1b4
cangzair0755 <cangzair0755@gmail.com>
2022-05-29 14:56:44 +0800

    cfefa18

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -12,16 +12,17 @@ else
 	exit 1
 fi
 [[ ! -f /tmp/Version_Tags ]] && echo "未检测到云端版本,请检查网络,或您的仓库为私库,或您修改的Github地址有错误,或发布已被删除,或再次刷新网页试试!" > /tmp/cloud_version && exit 1
-chmod +x /tmp/Version_Tags && source /tmp/Version_Tags
-if [[ -n "${CLOUD_Version}" ]];then
+source /tmp/Version_Tags
+chmod +x /tmp/Version_Tags
+if [[ ! -z "${CLOUD_Version}" ]];then
 	if [[ "${CURRENT_Version}" -eq "${CLOUD_Version}" ]];then
 		Checked_Type="已是最新"
 		echo "${CLOUD_Version} [${Checked_Type}]" > /tmp/cloud_version
 	elif [[ "${CURRENT_Version}" -gt "${CLOUD_Version}" ]];then
-		Checked_Type="发现更高版本固件可更新"
+		Checked_Type="发现更新"
 		echo "${CLOUD_Version} [${Checked_Type}]" > /tmp/cloud_version
 	elif [[ "${CURRENT_Version}" -lt "${CLOUD_Version}" ]];then
-		Checked_Type="云端最高版本固件,低于您现在所安装的版本"
+		Checked_Type="云端最高版本,低于您现在的版本"
 		echo "${CLOUD_Version} [${Checked_Type}]" > /tmp/cloud_version	
 	fi
 else
